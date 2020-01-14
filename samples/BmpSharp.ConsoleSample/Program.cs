12fe86ed2fc1412b86791020a9aa4bdde1eab25c
Drazen Soronda <dsoronda@gmail.com>
2020-01-14 20:07:52 +0000

    Move Gradient tests to Samples code

---
+++ samples/BmpSharp.ConsoleSample/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BmpSharp.ConsoleSample {
+	class Program {
+		static void Main( string[] args ) {
+			// NOTE : this will generate 
+			GradientExamples.RunSamples();
+			Console.WriteLine( "All done!" );
+		}
+	}
+}
